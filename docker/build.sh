e04476d61e4c5b14621fe1d9972a810f786df495
xwxztq <xwxztq@gmail.com>
2023-11-26 17:14:10 +0800

    :sparkles: docker doc upd, add submission part

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,22 @@
-# export requirements.txt
+# Informing the user that the script is exporting requirements.txt
+echo "Exporting requirements.txt..."
 pipreqs --force ./ --encoding=utf-8
+echo 
 
-# removing torch related packages
+# Informing the user about the removal of torch related packages
+echo "Removing torch related packages from requirements.txt..."
 grep -v -i "torch" requirements.txt > requirements.txt.tmp
 mv requirements.txt.tmp requirements.txt
+echo "Torch related packages removed."
+echo
 
-docker build -t submission .
+# Prompting the user to enter a number for the Docker build
+echo "Please enter the number of your team:"
+read number
 
+# Informing the user that Docker build is starting
+echo "Building Docker image named sub${number}..."
+docker build -t sub${number} .
+
+echo "Docker build completed."
+echo
